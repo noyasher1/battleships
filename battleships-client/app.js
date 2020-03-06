13577eb12e1c84d02c2a55bc73fd4c1735cf2c3e
nir <a>
2020-03-06 14:02:11 +0200

    Added onclick event for start button

--- battleships-client/app.js
+++ battleships-client/app.js
@@ -9,10 +9,15 @@ import BattleshipLocator from "./src/components/battleshipLocator.js";
 const BOARD_LENGTH = 10;
 const userBoardId = "user-board";
 const opponentBoardId = "opponent-board";
+const startButton = document.getElementById("startButton");
 
 const messageBox = new MessageBox();
 const userBoard = new UserBoard(userBoardId, BOARD_LENGTH, BOARD_LENGTH);
 const opponentBoard = new OpponentBoard(opponentBoardId, BOARD_LENGTH, BOARD_LENGTH);
+
+function addGameListeners(socket, buttonToRemove){
+    initializeBoardListeners(socket, messageBox, userBoard, buttonToRemove);
+}
 
 console.log("i am here");
 
@@ -20,5 +25,21 @@ window.addEventListener("beforeunload", (event) => {
     event.returnValue = ''; // When changing this value to value other then null or undefined, it prompt the message (in old browsers it prompt tje string set to the property)
 });
 
-const socket = io.connect("localhost:3000");
-initializeBoardListeners(socket, messageBox, BOARD_LENGTH, userBoard);
+messageBox.pushMessage("Please click on \"Find me an opponent\" button to start");
+
+let isStartButtonClicked = false;
+startButton.onclick = () => {
+    if(isStartButtonClicked){
+        return;
+    }
+    isStartButtonClicked = true;
+    startButton.disabled = true;
+    messageBox.popMessage();
+    messageBox.pushMessage("Looking for an opponent. Please wait.");
+    const socket = io.connect("localhost:3000");
+    addGameListeners(socket, startButton)
+};
+
+
+
+
