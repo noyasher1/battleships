e1bfb868cba46f43c9a65c9edd59b83f85dd959a
nir <a>
2020-03-17 11:38:12 +0200

    detect disconnection with the server in client side

--- battleships-client/src/events/disconnectionEvents/disconnectionHandlers.js
+++ battleships-client/src/events/disconnectionEvents/disconnectionHandlers.js
@@ -2,6 +2,16 @@
 import AlertModal from '../../components/alertModal.js'
 
 export default class DisconnectionHandlers {
+    static connectionError(serverSocket, beforeunloadFuncToAbort){
+        serverSocket.disconnect();
+        new AlertModal("Sorry :(\n"
+            + "There's not connection with the server."
+            + "Please try again later.", () => {
+            window.removeEventListener("beforeunload", beforeunloadFuncToAbort);
+            window.location.reload();
+        })
+    }
+
     static opponentHasDisconnected(beforeunloadFuncToAbort) {
         new AlertModal("Sorry :(\n"
                 + "Your opponent has left the game.\n"
--- battleships-client/src/events/disconnectionEvents/disconnectionListeners.js
+++ battleships-client/src/events/disconnectionEvents/disconnectionListeners.js
@@ -2,6 +2,10 @@
 import DisconnectionHandlers from "../disconnectionEvents/disconnectionHandlers.js";
 
 export default (socket, beforeunloadFunc) => {
+    socket.on("connect_error", () => {
+        DisconnectionHandlers.connectionError(socket, beforeunloadFunc);
+    });
+
     socket.on("OpponentHasDisconnected", () => {
         DisconnectionHandlers.opponentHasDisconnected(beforeunloadFunc);
     });
